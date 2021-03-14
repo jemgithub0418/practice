60e4c923bd3825eb8c1d0162e734e37e5427f284
Jeremi Ramos <jeremijaredramos@gmail.com>
2021-03-14 22:43:53 +0800

    Revert "added register form, but not yet finished, tweak add_register form in admin"
    This reverts commit 0783d3d449724bc44f57ed5baac361b2765f3925.

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,4 @@
 from django.shortcuts import render
-from forms import accounts
-
 
 def register(request):
-	userform = accounts.UserCreationForm()
-	if request.method == 'POST':
-		pass
-	else:
-		pass
-
-	context = {
-		'form': userform,
-	}
-
-	return render(request, 'accounts/register.html', context)
+	return render(request, 'accounts/register.html')
--- forms/accounts.py
+++ forms/accounts.py
@@ -3,17 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from accounts.models import User
 
 class UserCreationForm(UserCreationForm):
-	# first_name = forms.CharField(required=True)
-	def __init__(self, *args, **kwargs):
-		super(UserCreationForm, self).__init__(*args, **kwargs)
-
-		self.fields['first_name'].required = True
-		self.fields['last_name'].required = True
-		self.fields['email'].required = True
-
+	REQUIRED_FIELDS = ['first_name', 'last_name', 'email',]
 	class Meta:
 		model = User
-		fields = [
-				'username','password', 'password2', 'first_name', 'last_name', 
-				'email', 'address'
-			]
+		fields = '__all__'
