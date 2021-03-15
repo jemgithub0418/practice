9c558b2609f426d8784d1c1fbe53eb9424dd87b9
Jeremi Ramos <jeremijaredramos@gmail.com>
2021-03-15 20:18:49 +0800

    added blog and blogcategory table into admin page

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Blog, BlogCategory
 
-# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Blog)
+admin.site.register(BlogCategory)
