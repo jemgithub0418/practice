d6dfbaf63a19a3c18e127a6aedadb65e185baa59
Jeremi Ramos <jeremijaredramos@gmail.com>
2021-03-15 19:41:48 +0800

    asd

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,7 @@ from django.contrib import messages
 
 
 def register(request):
-    if request.method == "POST":
+    if registerequest.method == "POST":
         form = accounts.UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
