50bc71633a6865ef1b107f3e74975ae2c5c28113
Jeremi Ramos <jeremijaredramos@gmail.com>
2021-03-15 20:10:01 +0800

    added blogcategory table and blog table (not finished). migrate

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,7 @@ from django.contrib import messages
 
 
 def register(request):
-    if registerequest.method == "POST":
+    if request.method == "POST":
         form = accounts.UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
--- main/models.py
+++ main/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils.translation import ugettext as _
 
-# Create your models here.
+
+class BlogCategory(models.Model):
+    title = models.CharField(
+            max_length=256,
+            verbose_name = _('Title: '),
+            unique=True,
+            blank= False,
+            null= False
+        )
+
+    class Meta:
+        verbose_name = _('Blog_Categoty')
+        verbose_name_plural = _('Blog_Categories')
+
+    def __str__(self):
+        return self.title
+
+
+class Blog(models.Model):
+    title = models.CharField(
+            max_length = 256,
+            verbose_name = _('Title: '),
+            unique=True,
+            blank=False,
+            null=False,
+        )
+
+    slug = models.SlugField(
+            max_length=256,
+            verbose_name = _('Slug :'),
+            unique = True,
+            blank = False,
+            null = False
+        )
