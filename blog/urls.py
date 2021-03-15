79b0de0b315e1d555066db2c9c932c959e03a040
Jeremi Ramos <jeremijaredramos@gmail.com>
2021-03-15 15:39:46 +0800

    added home page, implemented namespaces for urls

--- blog/urls.py
+++ blog/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls', namespace = 'accounts')),
+    path('', include('main.urls', namespace = 'main')),
 ]
